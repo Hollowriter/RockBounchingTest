38a2688b0896997ade542ee36531f74a34c1e21b
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-07-14 12:40:18 -0300

    Rock pool added

---
+++ Assets/Scripts/Rock/RockPool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPool : SingletonBase<RockPool>
+{
+    [SerializeField]
+    GameObject instantiableRock;
+    GameObject[] rocks;
+    [SerializeField]
+    int poolSize;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        rocks = new GameObject[poolSize];
+    }
+
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    void FillRockPool() 
+    {
+        for (int i = 0; i<poolSize; i++) 
+        {
+            GameObject singleRock = (GameObject)Instantiate(instantiableRock, transform);
+            rocks[i] = singleRock;
+            rocks[i].SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        FillRockPool();
+    }
+}
