b52745ec2b44f7d8db495382d1b3d37f7fcf89df
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-07-17 15:52:11 -0300

    Coins dissappear after a time

---
+++ Assets/Scripts/Coin/CoinTimeDeactivator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTimeDeactivator : MonoBehaviour
+{
+    public float timeToDeactivate;
+    float time;
+
+    private void Awake()
+    {
+        time = 0;
+    }
+
+    void TimeTick() 
+    {
+        time += Time.deltaTime;
+    }
+
+    void DeactivateCoin() 
+    {
+        if (time >= timeToDeactivate) 
+        {
+            time = 0;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void Behave() 
+    {
+        TimeTick();
+        DeactivateCoin();
+    }
+
+    private void Update()
+    {
+        Behave();
+    }
+}
