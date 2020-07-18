9c8783fe55f7695e716c883f05ad56da2fc7f4c4
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-07-18 13:14:57 -0300

    UI Done

---
+++ Assets/Scripts/UI/UIDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDisplay : SingletonBase<UIDisplay>
+{
+    [SerializeField]
+    Text rockText;
+    [SerializeField]
+    Text coinText;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        rockText.text = "x" + 0;
+        coinText.text = "x" + 0;
+    }
+
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    void CheckInfoSources() 
+    {
+        rockText.text = "x" + RockCounter.instance.GetRocksInserted();
+        coinText.text = "x" + CoinCounter.instance.GetCoinsCollected();
+    }
+
+    protected override void BehaveSingleton()
+    {
+        CheckInfoSources();
+    }
+
+    private void Update()
+    {
+        BehaveSingleton();
+    }
+}
